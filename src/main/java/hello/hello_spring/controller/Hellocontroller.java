e4195fca2eaabd54e303960ef81dd99d7a7209df
andire120 <164429441+andire120@users.noreply.github.com>
2024-09-03 20:03:08 +0900

    spring

--- src/main/java/hello/hello_spring/controller/Hellocontroller.java
+++ src/main/java/hello/hello_spring/controller/Hellocontroller.java
@@ -1,8 +1,12 @@
 package hello.hello_spring.controller;
 
+import com.sun.source.tree.BreakTree;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Controller
 public class Hellocontroller {
@@ -13,4 +17,35 @@ public class Hellocontroller {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
