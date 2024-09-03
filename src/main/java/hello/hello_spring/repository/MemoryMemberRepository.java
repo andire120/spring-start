019ce18a32233860ae971b9e394c2e67aad5a499
andire120 <164429441+andire120@users.noreply.github.com>
2024-09-03 20:03:08 +0900

    spring

---
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.Map;
+import java.util.Optional;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    public static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId((++sequence));
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> finById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable((Member) store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
---
+++ src/test/java/hello/hello_spring/repository/memoryMemberRepositoryTest.java
@@ -0,0 +1,12 @@
+package hello.hello_spring.repository;
+
+import org.junit.jupiter.api.Test;
+
+public class memoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+
+    }
+}
