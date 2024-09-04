bda0c061aac1a330de9b4d8625198d5117361427
andire120 <164429441+andire120@users.noreply.github.com>
2024-09-04 19:23:08 +0900

    spring

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -1,4 +1,37 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member); //단축키 crtl + alt + shift + T 중복회원검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(member m -> {
+                    throw new IllegalAccessException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
